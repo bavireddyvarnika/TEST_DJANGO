f5a92f9a3d872a2b0859bab277524552b63fdb64
bavireddyvarnika <bavireddyvarnika@gmail.com>
2025-09-06 09:55:43 -0500

    commting view

--- test_app/views.py
+++ test_app/views.py
@@ -1,6 +1,13 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from .forms import AdditionForm
 
-# Create your views here.
-from django.http import HttpResponse
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+def add_numbers(request):
+    result = None
+    form = AdditionForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        a = form.cleaned_data["a"]
+        b = form.cleaned_data["b"]
+        result = a + b
+
+    return render(request, "add.html", {"form": form, "result": result})
